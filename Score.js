ddb7c0b46072f3e79ccd47dd34b2048415ea37a6
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-10-06 23:10:50 +0900

    Create Score.js

---
+++ Score.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const Score = (props) => {
+  const quiz_list = useSelector((state) => state.quiz.quiz_list);
+  const user_answer_list = useSelector((state) => state.quiz.user_answer_list);
+
+  const _score =
+    (100 / quiz_list.length) *
+    quiz_list.filter((q, idx) => {
+      return q.answer === user_answer_list[idx];
+    }).length;
+
+  const score = Math.round(_score);
+  return (
+    <div>
+      <h3>
+        {props.name} 퀴즈에 대한 내 점수는 <br />
+        {score}
+      </h3>
+
+      <p>우와! 우린 참 친해요!</p>
+
+      <button>{props.name}에게 한 마디</button>
+    </div>
+  );
+};
+
+export default Score;
