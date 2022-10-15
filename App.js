5510b828f84002fe45f0d72b12975aa320bfd94a
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-10-16 00:11:18 +0900

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import { Routes, Route } from "react-router-dom";
 import Start from "./Start";
 import Quiz from "./Quiz";
 import Score from "./Score";
-
+import Message from "./Message";
+import Ranking from "./Ranking";
 
 function App() {
   const [name, setName] = React.useState("Jiyeon");
@@ -22,6 +23,8 @@ function App() {
         <Route path="/" element={<Start name={name} />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/score" element={<Score name={name} />} />
+        <Route path="/message" element={<Message />} />
+        <Route path="/ranking" element={<Ranking />} />
       </Routes>
     </div>
   );
