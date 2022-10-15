7ecc943cef656c0fc19593859fd35a6b672f724b
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-10-16 00:12:47 +0900

    Update user.js

--- redux/modules/user.js
+++ redux/modules/user.js
@@ -1,15 +1,22 @@
 //user 이름을 넣자
 //이름을 바꿔준다
 
+//Action
 const SET_NAME = "user/SET_NAME";
+const SET_MESSAGE = "user/SET_MESSAGE";
 
 //action 생성함수
 export const setName = (name) => {
   return { type: SET_NAME, name };
 };
 
+export const setMessage = (message) => {
+  return { type: SET_MESSAGE, message };
+};
+
 const initialState = {
   user_name: "",
+  user_message: "",
 };
 
 export default function reducer(state = initialState, action = {}) {
@@ -18,6 +25,12 @@ export default function reducer(state = initialState, action = {}) {
       console.log(action);
       return { ...state, user_name: action.name };
     }
+
+    case "user/SET_MESSAGE": {
+      console.log(action);
+      return { ...state, user_message: action.message };
+    }
+
     default:
       return state;
   }
