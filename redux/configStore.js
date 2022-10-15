883c1f57d4539ef8826f68edc317f9ae474549ac
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-10-16 00:11:41 +0900

    Update configStore.js

--- redux/configStore.js
+++ redux/configStore.js
@@ -1,8 +1,9 @@
 import { combineReducers, createStore } from "redux";
 import quiz from "./modules/quiz";
 import user from "./modules/user";
+import ranking from "./modules/ranking";
 
-const rooReducer = combineReducers({ quiz, user });
+const rooReducer = combineReducers({ quiz, user, ranking });
 
 const store = createStore(rooReducer);
 
